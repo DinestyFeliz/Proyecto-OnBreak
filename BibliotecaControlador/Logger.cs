067810616c519cbfc9d16ff897cce5ae0f17804d
DinestyFeliz <felixeduardomarin@gmail.com>
2019-04-09 21:15:12 -0400

    Primer Commit Base

---
+++ BibliotecaControlador/Logger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;//entrada y salida
+
+namespace BibliotecaControlador
+{
+    public class Logger
+    {
+        public static void Mensaje(String msg)
+        {
+            msg = DateTime.Now + " | " + msg + Environment.NewLine;
+            File.AppendAllText(@"d:\logger.txt", msg);
+        }
+    }
+}
---
+++ vista/wpfListar.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using BibliotecaControlador;
+
+namespace vista
+{
+    /// <summary>
+    /// Lógica de interacción para wpfListar.xaml
+    /// </summary>
+    public partial class wpfListar : Window
+    {
+        public wpfListar()
+        {
+            InitializeComponent();
+            try
+            {
+                DaoPersona dao = new DaoPersona();
+                dgvListar.ItemsSource = dao.Lista();
+                dgvListar.Items.Refresh();
+            }
+            catch (Exception ex)
+            {
+
+                Logger.Mensaje(ex.Message);
+            }
+        }
+    }
+}
